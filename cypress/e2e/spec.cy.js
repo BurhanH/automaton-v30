7d8d68471a11f75f96df7fd949494c9499ee3108
Baur Urazalinov <BurhanH@users.noreply.github.com>
2025-05-21 13:25:37 -0500

    chore: initialize project with package.json and cypress dependency

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("Test", () => {
+  it("sample test1 ", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("sample test2", () => {
+    expect(true).to.equal(true);
+  });
+});
