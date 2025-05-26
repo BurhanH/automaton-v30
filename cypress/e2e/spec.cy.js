c3461173c29527c8e4cf334c7eab37753d871fbd
Baur Urazalinov <BurhanH@users.noreply.github.com>
2025-05-26 11:05:32 -0500

    Added simple test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,4 +8,12 @@ describe("Test", () => {
   it("sample test2", () => {
     expect(true).to.equal(true);
   });
+
+  it("add todos", () => {
+    cy.visit('https://example.cypress.io/');           // 60 seconds will wait for the page to load; transition event
+    cy.contains('type').click();                       // 4 seconds will wait for the element to be visible
+    cy.url().should('include', '/commands/actions');
+    cy.get('.action-email').type('fake@email.com');
+    cy.get('.action-email').should('have.value', 'fake@email.com');
+  });
 });
